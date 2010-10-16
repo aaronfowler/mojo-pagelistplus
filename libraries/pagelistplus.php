fa05d1867ceb8b1751da8dc9e36aceefdf914d08
Aaron Fowler <aaron@foggyhollow.net>
2010-10-16 11:35:35 -0400

    v1.1.1 fixed class/id bug

--- libraries/pagelistplus.php
+++ libraries/pagelistplus.php
@@ -137,7 +137,7 @@ class Pagelistplus
 		}
 		else // output the default page_list
 		{
-			$result = parser_page_list($this->site_structure);
+			$result = parser_page_list($this->site_structure, $attributes);
 		}
 		
 		if(!$result || is_numeric($result))
@@ -229,6 +229,33 @@ class Pagelistplus
 	}
 	
 
+	
+	/**
+	  * Debug Helper
+	  *
+	  * Outputs the given variable(s) with formatting and location
+	  *
+	  * @access        public
+	  * @param        mixed    variables to be output
+	  */
+	function dump()
+	{
+	    list($callee) = debug_backtrace();
+	    $arguments = func_get_args();
+	    $total_arguments = count($arguments);
+
+	    echo '<fieldset style="background: #fefefe !important; border:2px red solid; padding:5px">';
+	    echo '<legend style="background:lightgrey; padding:5px;">'.$callee['file'].' @ line: '.$callee['line'].'</legend><pre>';
+	    $i = 0;
+	    foreach ($arguments as $argument)
+	    {
+	        echo '<br/><strong>Debug #'.(++$i).' of '.$total_arguments.'</strong>: ';
+	        var_dump($argument);
+	    }
+
+	    echo "</pre>";
+	    echo "</fieldset>";
+	}
 }
 
 /* End of file pagelistplus.php */
